e330b6ce12cf3842c289c2149825cd622cf7ec0d
Fabio M. Di Nitto <fdinitto@redhat.com>
2009-01-07 12:17:29 +0100

    split tree into separate projects
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/cobj.mk
+++ make/cobj.mk
@@ -4,11 +4,3 @@
 # used by dlm/libdlm
 %_lt.o: $(S)/%.c
 	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $@ $<
-
-# used by rgmanager/src/daemons
-%-noccs.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(NOCCS_CFLAGS) -c -o $@ $<
-
-# used by fence/agents/xvm
-%-standalone.o: $(S)/%.c
-	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) $(STANDALONE_CFLAGS) -c -o $@ $<
