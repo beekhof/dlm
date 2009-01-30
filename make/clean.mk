e3e362f07b7a3e42f5f6216308319489d75d64f2
Fabio M. Di Nitto <fdinitto@redhat.com>
2009-01-30 10:56:37 +0100

    build: clean .pc files
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- make/clean.mk
+++ make/clean.mk
@@ -3,5 +3,5 @@ generalclean:
 	rm -rf core core.* .depend cscope.* *.orig *.rej
 	rm -rf linux .*.o.cmd .*.ko.cmd *.mod.c .tmp_versions
 	rm -rf Module.symvers Module.markers .*.o.d modules.order
-	rm -rf ${TARGET} ${TARGETS} ${TARGET}_test
+	rm -rf ${TARGET} ${TARGETS} ${TARGET}_test ${TARGET}.pc
 	rm -rf ${TARGET1} ${TARGET2} ${TARGET3} ${TARGET4} ${TARGET5} ${TARGET6}
